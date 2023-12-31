01e43675a4a81005de2d96aaacabb025b16ccf7f
jpassgo <jeffpascoe9@gmail.com>
2023-12-30 19:10:56 -0600

    Adding prettier for formatting

--- src/search/binarySearch.ts
+++ src/search/binarySearch.ts
@@ -1,22 +1,20 @@
-
-
 function binarySearch(target: number, arr: number[]): boolean {
-  const mid: number = Math.floor((arr.length - 1) / 2);
-  let result: boolean = false; 
+  const mid: number = Math.floor((arr.length - 1) / 2)
+  let result: boolean = false
 
   if (target === arr[mid]) {
-    return true;
+    return true
   } else if (target < arr[mid]) {
-    result = binarySearch(target, arr.slice(0, mid));
+    result = binarySearch(target, arr.slice(0, mid))
   } else if (target > arr[mid]) {
-    result = binarySearch(target, arr.slice(mid+1));
+    result = binarySearch(target, arr.slice(mid + 1))
   }
 
-  return result;
+  return result
 }
 
-const target: number = 3;
+const target: number = 3
 
-const arr: number[] = [1,2,3,4,5,6,7,8];
+const arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8]
 
-console.log(binarySearch(target, arr));
+console.log(binarySearch(target, arr))
