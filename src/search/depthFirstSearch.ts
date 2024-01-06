df0c477718ae4c5a0966d096b007f388f3ba3221
jpassgo <jeffpascoe9@gmail.com>
2024-01-06 14:58:28 -0600

    Modifying breadthFirstSearch

--- src/search/depthFirstSearch.ts
+++ src/search/depthFirstSearch.ts
@@ -3,22 +3,6 @@ import MyNode from '../dataStructures/MyNode';
 
 // For depth first search we will utilize a stack to help us step through each nodes children.
 //  We should be able to achieve this in O(n) since in the worst case we will visit each node once
-let one = new MyNode(1);
-let two = new MyNode(2);
-let three = new MyNode(3);
-three.addChild(one);
-three.addChild(two);
-
-let four = new MyNode(4);
-let five = new MyNode(5);
-let six = new MyNode(6);
-six.addChild(four);
-six.addChild(five);
-
-let root = new MyNode(10);
-root.addChild(three);
-root.addChild(six);
-
 
 function depthFirstSeach(root: MyNode, value: number): boolean {
   let stack: Stack<MyNode> = new Stack<MyNode>();
@@ -38,6 +22,22 @@ function depthFirstSeach(root: MyNode, value: number): boolean {
   
   return false;
 }
+
+let one = new MyNode(1);
+let two = new MyNode(2);
+let three = new MyNode(3);
+three.addChild(one);
+three.addChild(two);
+
+let four = new MyNode(4);
+let five = new MyNode(5);
+let six = new MyNode(6);
+six.addChild(four);
+six.addChild(five);
+
+let root = new MyNode(10);
+root.addChild(three);
+root.addChild(six);
 
 console.log(depthFirstSeach(root, 2));
 console.log(depthFirstSeach(root, 17));
